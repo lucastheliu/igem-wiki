1c37bdf1a13b8eb3b0faa485e3aea730a1fbbc3c
LucasTL <lucasliuhaolin@gmail.com>
2025-05-06 17:34:00 +0200

    feat: init commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lucastheliu.github.io",
+  base: "igem-wiki",
+});
