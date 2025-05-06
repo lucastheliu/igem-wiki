314e86bd4ccdb65bff739484838098bd67872981
LucasTL <lucasliuhaolin@gmail.com>
2025-05-06 17:53:12 +0200

    feat: add tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucastheliu.github.io",
   base: "igem-wiki",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
